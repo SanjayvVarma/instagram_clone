0951e99d94f83eae09f53cd641218a79fffdf9e8
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-02-07 13:15:01 +0530

    sanjay

---
+++ Sanjay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Sanjay = () => {
+    return (
+        <View>
+            <Text>Hello, I am Sanjay Component!</Text>
+        </View>
+    );
+};
+
+export default Sanjay;
