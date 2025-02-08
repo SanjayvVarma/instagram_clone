0968643c07e7d365066e2bba7a31c227e4e4da07
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-02-08 09:34:44 +0530

    Replace Sanjay component with InputBox and update App.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Sanjay from './Sanjay';
+import InputBox from './src/components/InputBox';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Sanjay />
-      <Text>Open up App.js to start working on your app!</Text>
+      <InputBox />
     </View>
   );
 }
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const InputBox = () => {
+    return (
+        <View style={styles.container}>
+            <Text>InputBox</Text>
+        </View>
+    )
+}
+
+export default InputBox
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        height: 68
+    }
+})
