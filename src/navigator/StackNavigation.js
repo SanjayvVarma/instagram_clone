bc7552b682c4b5758811a6c1abfbfdcca580dc48
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-02-10 20:46:13 +0530

    Replace Header component with Dashboard in StackNavigation for improved home screen functionality

--- src/navigator/StackNavigation.js
+++ src/navigator/StackNavigation.js
@@ -4,7 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Login from '../screen/auth/Login'
 import Signup from '../screen/auth/Signup'
-import Header from '../components/Header'
+import Dashboard from '../screen/dashboard/Dashboard'
 
 
 const StackNavigation = () => {
@@ -16,7 +16,7 @@ const StackNavigation = () => {
             <Stack.Navigator screenOptions={{ headerShown: false }}>
                 <Stack.Screen name='Login' component={Login} />
                 <Stack.Screen name='Signup' component={Signup} />
-                <Stack.Screen name='Home' component={Header} />
+                <Stack.Screen name='Home' component={Dashboard} />
             </Stack.Navigator>
         </NavigationContainer>
     )
