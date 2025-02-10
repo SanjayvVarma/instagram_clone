49dc55b84ff6110b0bb6e49494a8af531096f55b
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-02-10 20:10:31 +0530

    Add Stories component and integrate it into the Dashboard for enhanced content display

---
+++ src/components/Stories.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Stories = () => {
+  return (
+    <View>
+      <Text style={{ backgroundColor: 'red', textAlign: 'center' }}>Stories this is hsh</Text>
+    </View>
+  )
+}
+
+export default Stories
+
+const styles = StyleSheet.create({})
--- src/screen/dashboard/Dashboard.jsx
+++ src/screen/dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Header from '../../components/Header'
+import Stories from '../../components/Stories'
 
 const Dashboard = () => {
     return (
         <View>
             <Header />
+            <Stories />
         </View>
     )
 }
