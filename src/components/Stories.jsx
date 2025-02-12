c6bd08598b4346bd145328d80c4cd18a0fa47b7f
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-02-12 09:52:25 +0530

    Enhance Stories component with user data display and navigation functionality

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,15 +1,51 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
 import React from 'react'
+import UserData from '../utils/UserData'
+import { useNavigation } from '@react-navigation/native'
 
 const Stories = () => {
-  console.log("✅ Stories Component Loaded");
+
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>Stories</Text>
+    <View style={styles.mainContainer}>
+      {
+        UserData.map((item) => (
+          <View key={item.id} style={styles.container}>
+            <TouchableOpacity onPress={() => navigation.navigate('StoryView', { item })}>
+              <View style={styles.imgView}>
+                <Image style={styles.img} source={item.story.image} />
+              </View>
+            </TouchableOpacity>
+            <Text style={{ textAlign: 'center' }}>{item.username}</Text>
+          </View>
+        ))
+      }
     </View>
   )
 }
 
 export default Stories
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  mainContainer: {
+    flexDirection: 'row',
+    marginTop: 7
+  },
+
+  container: {
+    marginLeft: 10
+  },
+
+  imgView: {
+    borderWidth: 3,
+    borderRadius: 40,
+    padding: 2
+  },
+
+  img: {
+    height: 70,
+    width: 70,
+    borderRadius: 35
+  }
+})
