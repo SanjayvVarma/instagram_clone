6748dfbb4209be6ef3aa1feb786fdf4e98a32166
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-02-16 09:47:07 +0530

    Add Header, Stories, and StoryView components to App; adjust container margin

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native';
 import StackNavigation from './src/navigator/StackNavigation';
+import Header from './src/components/Header';
+import Stories from './src/components/Stories';
+import StoryView from './src/components/StoryView';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StackNavigation />
+      {/* <Header />
+      <Stories /> */}
+      {/* <StoryView /> */}
     </View>
   );
 }
@@ -12,5 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 40
   },
 });
