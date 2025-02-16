651efb21d5872c385414ea273e27a6bf16c16bca
SanjayvVarma <spverma.sanjay56056@gmail.com>
2025-02-16 09:46:31 +0530

    Add BottomNavigation component and integrate it into StackNavigation

--- src/navigator/StackNavigation.js
+++ src/navigator/StackNavigation.js
@@ -6,6 +6,7 @@ import Login from '../screen/auth/Login'
 import Signup from '../screen/auth/Signup'
 import Dashboard from '../screen/dashboard/Dashboard'
 import StoryView from '../components/StoryView'
+import BottomNavigation from './BottomNavigation'
 
 
 const StackNavigation = () => {
@@ -19,6 +20,7 @@ const StackNavigation = () => {
                 <Stack.Screen name='Signup' component={Signup} />
                 <Stack.Screen name='Home' component={Dashboard} />
                 <Stack.Screen name='StoryView' component={StoryView} />
+                <Stack.Screen name='Dashboard' component={BottomNavigation} />
             </Stack.Navigator>
         </NavigationContainer>
     )
